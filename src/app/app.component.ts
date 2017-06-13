259524dd6c3a300e3c67eadacaa93ad87fb9c126
Balwinder Katoch <vickykatoch@yahoo.com>
2017-06-12 23:28:17 -0400

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LibService } from 'f-gui-core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  meaning: number;
+
+  constructor(libService: LibService) {
+    this.meaning = libService.getMeaning();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { LibModule } from 'f-gui-core';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LibModule
   ],
   providers: [],
   bootstrap: [AppComponent]

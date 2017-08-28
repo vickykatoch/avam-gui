944aaaacea7b1c552ee4ca0b7b82da2dc6d64fc0
Balwinder Katoch <vickykatoch@yahoo.com>
2017-08-28 05:14:51 -0400

    quick sort

--- src/app/sorter-algos/services/mergeSort.ts
+++ src/app/sorter-algos/services/mergeSort.ts
@@ -6,6 +6,33 @@ import { ISorter, IComparable } from "./contracts";
 export class MergeSort<T extends IComparable<T>> extends PerformanceTracker<T> implements ISorter<T> {
 
     sort(items: T[]): T[] {
-        throw new Error("Method not implemented.");
+        if (items.length > 1) {
+            const left = Math.floor(items.length/2);
+            const right = items.length-left;
+            const leftItems = items.slice(0,left);
+            const rightItems = items.slice(left);
+            this.sort(leftItems);
+            this.sort(rightItems);
+            this.merge(items,leftItems,rightItems)
+        }
+        return items;
+    }
+    private merge(items: T[],left: T[],right: T[]) {
+        let remaining = left.length + right.length;
+        let lIdx = 0, rIdx = 0, targetIdx = 0;
+
+        while(remaining>0) {
+            if(lIdx >= left.length) {
+                this.assign(items,targetIdx,right[rIdx++]);
+            } else if(rIdx >= right.length) {
+                this.assign(items,targetIdx,left[lIdx++]);
+            } else if(this.compare(left[lIdx], right[rIdx]) < 0) {
+                this.assign(items,targetIdx,left[lIdx++]);
+            } else {
+                this.assign(items,targetIdx,right[rIdx++]);
+            }
+            targetIdx++;
+            remaining--;
+        }
     }
 }
--- src/app/sorter-algos/services/quickSort.ts
+++ src/app/sorter-algos/services/quickSort.ts
@@ -6,6 +6,82 @@ import { ISorter, IComparable } from "./contracts";
 export class QuickSort<T extends IComparable<T>> extends PerformanceTracker<T> implements ISorter<T> {
 
     sort(items: T[]): T[] {
-        throw new Error("Method not implemented.");
+        if(items.length>0) {
+            this.quickSort(items,0, items.length-1)
+        }        
+        return items;
+    }
+    private quickSort(items: T[],left : number, right : number) : void {
+        if(left<right) {
+            debugger;
+            const pivot = this.getRandomNum(left,right);
+            const newPivot = this.partition(items, left,right,pivot);
+            this.quickSort(items,left, newPivot-1);
+            this.quickSort(items,newPivot+1, right);
+        }
+    }
+    private partition(items: T[],left : number, right : number, pivot : number) : number {
+        const pivotValue = items[pivot];
+            this.swap(items,pivot,right);
+            let storeIndex = left;
+            for(let i=left;i<right;i++) {
+                if(this.compare(items[i],pivotValue) <0) {
+                    this.swap(items,i,storeIndex);
+                    storeIndex++;
+                }
+            }
+            this.swap(items,storeIndex, right);
+            return storeIndex;
+    }
+    private getRandomNum(from: number, to : number) : number {
+        return Math.floor((Math.random() * to) + from);
     }
 }
+
+/*
+
+public class QuickSort<T> : Tracker<T>, ISorter<T>
+        where T : IComparable<T>
+    {
+        public void Sort(T[] items)
+        {
+            quicksort(items, 0, items.Length - 1);
+        }
+
+        private void quicksort(T[] items, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivotIndex = _pivotRng.Next(left, right);
+                int newPivot = partition(items, left, right, pivotIndex);
+
+                quicksort(items, left, newPivot - 1);
+                quicksort(items, newPivot + 1, right);
+            }
+        }
+
+        private int partition(T[] items, int left, int right, int pivotIndex)
+        {
+            T pivotValue = items[pivotIndex];
+
+            Swap(items, pivotIndex, right);
+
+            int storeIndex = left;
+
+            for (int i = left; i < right; i++)
+            {
+                if (Compare(items[i], pivotValue) < 0)
+                {
+                    Swap(items, i, storeIndex);
+                    storeIndex += 1;
+                }
+            }
+
+            Swap(items, storeIndex, right);
+            return storeIndex;
+        }
+
+        Random _pivotRng = new Random();
+    }
+
+    */
